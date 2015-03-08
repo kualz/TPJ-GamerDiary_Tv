f48b34120b4e62a5d611f0f8878dae1acd56410b
Jorge Paiva <fskjorge@gmail.com>
2015-03-08 22:04:16 +0000

    STUFF ADDED
    UPDATE 0.1
    -Agora o pac-man já esta a virar a boca para o lado certo.
    -sistema de pontuaçao adicionado.
    -Forma de ganhar já esta ativa.
    -Comida Bonus já está presente mas sem efeito
    -Comida espalhada pelo mapa

--- PAC-Man0.0.1/PAC-Man/Game1.cs
+++ PAC-Man0.0.1/PAC-Man/Game1.cs
@@ -14,59 +14,78 @@ namespace PAC_Man
     
     public class Game1 : Game
     {
+        GameState gamestate;
+        enum GameState
+        {
+            Win,
+            running,
+            gameOver,
+            LostLife
+        };
+        bool down = false, up = false, left, right = false;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D square;
         Texture2D PacMan;
-        Player PacMan_Loc = new Player(1, 1);
-        Keys key;
+        Texture2D PacManDown;
+        Texture2D PacManLeft;
+        Texture2D FOOD;
+        Texture2D FOOD1;
+        Texture2D PacManUp;
+        SpriteFont Score;
+        Player PacMan_Loc = new Player(14, 11);
+        int score = 0;
         float lastHumanMove;
         byte[,] board = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-                         {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-                         {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-                         {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-                         {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-                         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-                         {1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
-                         {1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
-                         {1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
-                         {1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,0,1,1,1,2,2,1,1,1,0,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,0,1,2,2,2,2,2,2,1,0,1,1,0,1,1,1,1,1,1},
-                         {3,0,0,0,0,0,0,0,0,0,1,2,2,2,2,2,2,1,0,0,0,0,0,0,0,0,0,3},
-                         {1,1,1,1,1,1,0,1,1,0,1,2,2,2,2,2,2,1,0,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
-                         {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
-                         {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-                         {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-                         {1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-                         {1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1},
-                         {1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
-                         {1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
-                         {1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
-                         {1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
-                         {1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
-                         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                         {1,5,5,5,5,5,5,5,5,5,5,5,5,1,1,5,5,5,5,5,5,5,5,5,5,5,5,1},
+                         {1,5,1,1,1,1,5,1,1,1,1,1,5,1,1,5,1,1,1,1,1,5,1,1,1,1,5,1},
+                         {1,5,1,1,1,1,5,1,1,1,1,1,5,1,1,5,1,1,1,1,1,5,1,1,1,1,5,1},
+                         {1,5,1,1,1,1,5,1,1,1,1,1,5,1,1,5,1,1,1,1,1,5,1,1,1,1,5,1},
+                         {1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,1},
+                         {1,5,1,1,1,1,5,1,1,5,1,1,1,1,1,1,1,1,5,1,1,5,1,1,1,1,5,1},
+                         {1,5,1,1,1,1,5,1,1,5,1,1,1,1,1,1,1,1,5,1,1,5,1,1,1,1,5,1},
+                         {1,5,5,5,5,5,5,1,1,5,5,5,5,1,1,5,5,5,5,1,1,5,5,5,5,5,5,1},
+                         {1,1,1,1,1,1,5,1,1,1,1,1,0,1,1,0,1,1,1,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,1,1,1,0,1,1,0,1,1,1,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,0,0,0,0,0,0,0,0,0,0,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,0,1,1,1,2,2,1,1,1,0,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,0,1,2,2,2,2,2,2,1,0,1,1,5,1,1,1,1,1,1},
+                         {3,0,0,0,0,0,5,0,0,0,1,2,2,2,2,2,2,1,0,0,0,5,0,0,0,0,0,3},
+                         {1,1,1,1,1,1,5,1,1,0,1,2,2,2,2,2,2,1,0,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,0,1,1,1,1,1,1,1,1,0,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,0,0,0,0,4,0,0,0,0,0,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,0,1,1,1,1,1,1,1,1,0,1,1,5,1,1,1,1,1,1},
+                         {1,1,1,1,1,1,5,1,1,0,1,1,1,1,1,1,1,1,0,1,1,5,1,1,1,1,1,1},
+                         {1,5,5,5,5,5,5,5,5,5,5,5,5,1,1,5,5,5,5,5,5,5,5,5,5,5,5,1},
+                         {1,5,1,1,1,1,5,1,1,1,1,1,5,1,1,5,1,1,1,1,1,5,1,1,1,1,5,1},
+                         {1,5,1,1,1,1,5,1,1,1,1,1,5,1,1,5,1,1,1,1,1,5,1,1,1,1,5,1},
+                         {1,5,5,5,1,1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,1,1,5,5,5,1},
+                         {1,1,1,5,1,1,5,1,1,5,1,1,1,1,1,1,1,1,5,1,1,5,1,1,5,1,1,1},
+                         {1,1,1,5,1,1,5,1,1,5,1,1,1,1,1,1,1,1,5,1,1,5,1,1,5,1,1,1},
+                         {1,5,5,5,5,5,5,1,1,5,5,5,5,1,1,5,5,5,5,1,1,5,5,5,5,5,5,1},
+                         {1,5,1,1,1,1,1,1,1,1,1,1,5,1,1,5,1,1,1,1,1,1,1,1,1,1,5,1},
+                         {1,5,1,1,1,1,1,1,1,1,1,1,5,1,1,5,1,1,1,1,1,1,1,1,1,1,5,1},
+                         {1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,1},
                          {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
-                       
+
+        byte[,] newBoard = new byte[28, 31];
+       
         public Game1()
             : base()
         {
             graphics = new GraphicsDeviceManager(this);
             graphics.IsFullScreen = false;
-            graphics.PreferredBackBufferHeight = 620;
+            graphics.PreferredBackBufferHeight = 650;
             graphics.PreferredBackBufferWidth = 560;
             Content.RootDirectory = "Content";
+            newBoard = board;
         }
 
         
         protected override void Initialize()
         {
             base.Initialize();
+            gamestate = GameState.running;
         }
 
         
@@ -74,7 +93,15 @@ namespace PAC_Man
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             square = Content.Load<Texture2D>("square");
+
             PacMan = Content.Load<Texture2D>("Bitmap1");
+            PacManDown = Content.Load<Texture2D>("PacManDown");
+            PacManUp = Content.Load<Texture2D>("PacManUp");
+            PacManLeft = Content.Load<Texture2D>("PacManLeft");
+
+            FOOD = Content.Load<Texture2D>("Bitmap2");
+            Score = Content.Load<SpriteFont>("MyFont");
+            FOOD1 = Content.Load<Texture2D>("Bitmap3");
         }
 
         
@@ -82,6 +109,11 @@ namespace PAC_Man
         {
             square.Dispose();
             PacMan.Dispose();
+            PacManDown.Dispose();
+            PacManLeft.Dispose();
+            PacManUp.Dispose();
+            FOOD.Dispose();
+            FOOD1.Dispose();
         }
 
         
@@ -90,84 +122,159 @@ namespace PAC_Man
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             lastHumanMove += (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (lastHumanMove >= 0.5f / 10f)
+            if (gamestate == GameState.running)
             {
-                lastHumanMove = 0;
-                if (Keyboard.GetState().IsKeyDown(Keys.Down))
-                    if (CanGoDown())
-                        PacMan_Loc.Incrementa_PY();
-                if (Keyboard.GetState().IsKeyDown(Keys.Up))
-                    if (CanGoUp())
-                        PacMan_Loc.Decrementa_PY();
-                if (Keyboard.GetState().IsKeyDown(Keys.Left))
-                    if (CanGoleft())
+                if (lastHumanMove >= 0.5f / 10f)
+                {
+                    lastHumanMove = 0;
+                    if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                        if (CanGoDown())
+                        {
+                            PacMan_Loc.Incrementa_PY();
+                            down = true;
+                            up = false;
+                            left = false;
+                            right = false;
+                        }
+
+                    if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                        if (CanGoUp())
+                        {
+                            PacMan_Loc.Decrementa_PY();
+                            up = true;
+                            down = false;
+                            left = false;
+                            right = false;
+                        }
+                    if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                        if (CanGoleft())
                         //if (board[PacMan_Loc._Py(), PacMan_Loc._Px()] == 3)
                         //    PacMan_Loc.Change_Py(29);
-                        PacMan_Loc.Decrementa_PX();
-                if (Keyboard.GetState().IsKeyDown(Keys.Right))
-                    if (CanGoRight())
+                        {
+                            PacMan_Loc.Decrementa_PX();
+                            left = true;
+                            up = false;
+                            down = false;
+                            right = false;
+                        }
+                    if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                        if (CanGoRight())
                         //if (board[PacMan_Loc._Py(), PacMan_Loc._Px()] == 3)
                         //    PacMan_Loc.Change_Py(0);
-                        PacMan_Loc.Incrementa_PX();
-
+                        {
+                            PacMan_Loc.Incrementa_PX();
+                            right = true;
+                            up = false;
+                            down = false;
+                            left = false;
+                        }
+                }
+                if (board[PacMan_Loc._Py(), PacMan_Loc._Px()] == 5)
+                {
+                    score += 2;
+                    board[PacMan_Loc._Py(), PacMan_Loc._Px()] = 0;
+                }
+                if (board[PacMan_Loc._Py(), PacMan_Loc._Px()] == 4)
+                {
+                    score += 10;
+                    board[PacMan_Loc._Py(), PacMan_Loc._Px()] = 0;
+                }
+                GameOver();
             }
-
-
-            base.Update(gameTime);
+            if (gamestate == GameState.LostLife)
+            {
+                PacMan_Loc.change_Px(14);
+                PacMan_Loc.Change_Py(11);
+                board = newBoard;
+            }
+            base.Update(gameTime); 
         }
-
         
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
             spriteBatch.Begin();
+
+
+            spriteBatch.DrawString(Score, "Score: " + score, new Vector2(10, 620), Color.White);
             for (int x = 0; x < 28; x++)
                 for (int y = 0; y < 31; y++)
                 {
                     if (board[y, x] == 0)
-                    {
                         if (PacMan_Loc._Px() == x && PacMan_Loc._Py() == y)
                         {
-                            spriteBatch.Draw(PacMan, new Vector2(x * 20, y * 20), Color.White);
+                            if (down)
+                                spriteBatch.Draw(PacManDown, new Vector2(x * 20, y * 20), Color.White);
+                            if (up)
+                                spriteBatch.Draw(PacManUp, new Vector2(x * 20, y * 20), Color.White);
+                            if (left)
+                                spriteBatch.Draw(PacManLeft, new Vector2(x * 20, y * 20), Color.White);
+                            if (right)
+                                spriteBatch.Draw(PacMan, new Vector2(x * 20, y * 20), Color.White);
                         }
-                    }
-                    if (board[y, x] ==1 )
-                    {
+                    if (board[y, x] == 1)
                         spriteBatch.Draw(square, new Vector2(x * 20, y * 20), Color.Blue);
-                    }
                     if (board[y, x] == 2)
-                    {
                         spriteBatch.Draw(square, new Vector2(x * 20, y * 20), Color.White);
-                    }
+                    if (board[y, x] == 4)
+                        spriteBatch.Draw(FOOD, new Vector2(x * 20, y * 20), Color.White);
+                    if (board[y, x] == 5)
+                        spriteBatch.Draw(FOOD1, new Vector2(x * 20, y * 20), Color.White);
                 }
+            if (gamestate == GameState.Win)
+            {
+                spriteBatch.DrawString(Score, "Good Job", new Vector2(220, 260), Color.White);
+                spriteBatch.DrawString(Score, "Score: " + score, new Vector2(220, 275), Color.White);
+                UnloadContent();
+            }
             spriteBatch.End();
+
             base.Draw(gameTime);
         }
 
         private bool CanGoleft()
         {
-            if (board[PacMan_Loc._Py(), PacMan_Loc._Px() - 1] == 0 )
+            if (board[PacMan_Loc._Py(), PacMan_Loc._Px() - 1] == 0 || 
+                board[PacMan_Loc._Py(), PacMan_Loc._Px() - 1] == 4 ||
+                board[PacMan_Loc._Py(), PacMan_Loc._Px() - 1] == 5)
                 return true;
             else return false;
         }
         private bool CanGoRight()
         {
-            if (board[PacMan_Loc._Py(), PacMan_Loc._Px() + 1] == 0 )
+            if (board[PacMan_Loc._Py(), PacMan_Loc._Px() + 1] == 0 || 
+                board[PacMan_Loc._Py(), PacMan_Loc._Px() + 1] == 4 ||
+                board[PacMan_Loc._Py(), PacMan_Loc._Px() + 1] == 5)
                 return true;
             else return false;
         }
         private bool CanGoUp()
         {
-            if (board[PacMan_Loc._Py() - 1, PacMan_Loc._Px()] == 0)
+            if (board[PacMan_Loc._Py() - 1, PacMan_Loc._Px()] == 0 || 
+                board[PacMan_Loc._Py() - 1, PacMan_Loc._Px()] == 5)
                 return true;
             else return false;
         }
         private bool CanGoDown()
         {
-            if (board[PacMan_Loc._Py() + 1, PacMan_Loc._Px()] == 0)
+            if (board[PacMan_Loc._Py() + 1, PacMan_Loc._Px()] == 0 ||
+                board[PacMan_Loc._Py() + 1, PacMan_Loc._Px()] == 5)
                 return true;
             else return false;
+        }
+        private void GameOver()
+        {
+            int aux = 0;
+            for (int x = 0; x < 28; x++)
+                for (int y = 0; y < 31; y++)
+                {
+                    if (board[y, x] == 5)
+                        aux++;
+                }
+            if (aux != 0)
+                gamestate = GameState.running;
+            else
+                gamestate = GameState.Win;
         }
     }
 }
